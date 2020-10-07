3d8bc7b730d71159c1637bf80ea9201ee0c6844e
jollymar <arun.jollymarlier@mail.utoronto.ca>
2020-10-07 19:04:45 -0400

    Testing cors work around and starting page

--- app.py
+++ app.py
@@ -6,32 +6,36 @@ app = Flask(__name__, static_folder="/", static_url_path="/")
 
 @app.route("/")
 def index():
-	return "YOU WEREN'T SUPPOSED TO SEE THIS"
+	#return "YOU WEREN'T SUPPOSED TO SEE THIS"
+    return app.send_static_file('index.html')
 
 #Returns discountCode data from local json file
-@app.route('/api/getDiscountCodes')
+@app.route('/api/getDiscountCodes', methods=['GET'])
 def get_discount_codes():
     with open('./data/discountCodes.json') as f:
         data = json.load(f)
-        
+
+    data.headers.add('Access-Control-Allow-Origin', '*')       
     return json.dumps(data)
 
 
 #Returns inventory data from local json file
-@app.route('/api/getInventory')
+@app.route('/api/getInventory', methods=['GET'])
 def get_inventory():
     with open('./data/inventory.json') as f:
         data = json.load(f)
-        
+
+    data.headers.add('Access-Control-Allow-Origin', '*')
     return json.dumps(data)
 
 
 #Returns saved Cart Items
-@app.route('/api/getCartItems')
+@app.route('/api/getCartItems', methods=['GET'])
 def get_cart_items():
     with open('./data/cartItems.json') as f:
         data = json.load(f)
     
+    data.headers.add('Access-Control-Allow-Origin', '*')
     return json.dumps(data)
 
 
