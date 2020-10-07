5022f39ceaa4e8bbc5e136424fa9f36f1bb95446
jollymar <arun.jollymarlier@mail.utoronto.ca>
2020-10-07 18:16:30 -0400

    make it better

---
+++ app.py
@@ -0,0 +1,39 @@
+import json
+import os
+from flask import Flask
+
+app = Flask(__name__, static_folder="/", static_url_path="/")
+
+@app.route("/")
+def index():
+	return "Hello"
+
+#Returns discountCode data from local json file
+@app.route('/api/getDiscountCodes')
+def get_discount_codes():
+    with open('./data/discountCodes.json') as f:
+        data = json.load(f)
+        
+    return json.dumps(data)
+
+
+#Returns inventory data from local json file
+@app.route('/api/getInventory')
+def get_inventory():
+    with open('./data/inventory.json') as f:
+        data = json.load(f)
+        
+    return json.dumps(data)
+
+
+#Returns saved Cart Items
+@app.route('/api/getCartItems')
+def get_cart_items():
+    with open('./data/cartItems.json') as f:
+        data = json.load(f)
+    
+    return json.dumps(data)
+
+
+if __name__ == "__main__":
+    app.run()
