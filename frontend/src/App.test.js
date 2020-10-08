249449b33798ce833762dca1ff0c601de4429125
jollymar <arun.jollymarlier@mail.utoronto.ca>
2020-10-08 19:41:03 -0400

    fixed proxy error

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import PriceDisplay from './PriceDisplay';
 
 it('App renders without crashing', () => {
   const div = document.createElement('div');
@@ -8,28 +9,39 @@ it('App renders without crashing', () => {
 });
 
 it("getInventory connection successful", () => {
-  fetch(`${window.origin}api/getInventory`)
-    .then(res => {
-      if(res.status !== 200){
-        throw Error
-      }
-    }).catch(() => {throw Error})
-})
+  let testCartItems = [
+    {"name": "Roasted Peanuts", "price": 5.99, "amount": 2}, 
+    { "name": "Sliced Pineapple", "price": 4.99, "amount": 1}
+  ]
 
-it("getCartItems connection successful", () => {
-  fetch(`${window.origin}api/getCartItems`)
-    .then(res => {
-      if(res.status !== 200){
-        throw Error
-      }
-    }).catch(() => {throw Error})
-})
+  let testDiscounts = [
+    {"code": "TENOFF", "discountRate": 0.10},
+    {"code": "SWEET15", "discountRate": 0.15},
+    {"code": "TWENTYTWENTY", "discountRate": 0.20},
+    {"code": "HEYTHIRTY", "discountRate": 0.30},
+    {"code": "HALFSIES", "discountRate": 0.50},
+    {"code": "ALMOSTFREE", "discountRate": 0.75}
+]
 
-it("getDiscountCodes connection successful", () => {
-  fetch(`${window.origin}api/getDiscountCodes`)
-    .then(res => {
-      if(res.status !== 200){
-        throw Error
-      }
-    }).catch(() => {throw Error})
+  let testDiscountRate = 0.2
+  let testTotalPrice = 16.97
+
+  testUpdateDiscounts = () => {
+    console.log("Discount Updated")
+  }
+
+  const div = document.createElement('div');
+  ReactDOM.render(<PriceDisplay
+                    id="testPriceDisplay"
+                    discounts={testDiscounts}
+                    updateDiscount={testUpdateDiscount} 
+                    totalPrice={testTotalPrice}
+                    discountRate={testDiscountRate}
+                    cartItems={testCartItems}/>
+    , div);
+
+   
+
+  expect(testTotalPrice)
+
 })
